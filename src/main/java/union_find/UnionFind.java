35c1a87dc48bd1f192230696b23f38d9c5c29b98
katmeapp <el20432@mail.ntua.gr>
2023-10-21 23:47:06 +0300

    Add first version, unoptimized

--- src/main/java/union_find/UnionFind.java
+++ src/main/java/union_find/UnionFind.java
@@ -3,8 +3,26 @@
  */
 package union_find;
 
-public class Library {
-    public boolean someLibraryMethod() {
-        return true;
+public class UnionFind {
+    public UnionFind (int n){
+      size = n;
+      parent = new int[size];
+      for (int i = 0; i < size; i++)
+        parent[i]=i;
     }
+
+    public int find(int u){
+      while(true){
+        int p =parent[u];
+        if (p==u) return u;
+        u =p;
+      }
+    }
+    public void union(int u, int v){
+      int ru = find(u);
+      int rv = find(v);
+      if (ru != rv) parent[ru] = rv;
+    }
+    private int size;
+    private int[] parent;
 }
